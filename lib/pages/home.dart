b273c2a44e3ab4fa542229e6b9205740e05ae1d3
AurelioAlfons <yuroalfons0407@gmail.com>
2024-09-24 14:56:56 +1000

    Remove debug banner & add class

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,1 +1,18 @@
+// Author: Aurelio Hevi Alfons
+// Simple Food App
 
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // const - constant it means value cannot change & created only once
+    return const MaterialApp(
+      // Remove the debug banner on top right
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
