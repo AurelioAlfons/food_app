70928bac5bfacfa377d0a15be211ac8965f3a674
AurelioAlfons <yuroalfons0407@gmail.com>
2024-09-24 16:32:20 +1000

    Extract appBar  to clean

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,30 +11,55 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // const - constant it means value cannot change & created only once
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          '🍔Breakfast🍕',
-          style: TextStyle(
-              color: Color.fromARGB(255, 255, 255, 255),
-              fontSize: 24,
-              fontWeight: FontWeight.bold),
+      // Extract the appBar function()
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        '🍔Breakfast🍕',
+        style: TextStyle(
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+      // Change App bar bg color
+      backgroundColor: const Color.fromARGB(255, 81, 61, 236),
+      // Remove the shadow of the app bar
+      elevation: 0.0,
+      // Center the text
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: const Color(0x000000ff),
+              borderRadius: BorderRadius.circular(10)),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            // ignore: deprecated_member_use
+            color: Colors.white,
+            height: 30,
+            width: 30,
+          ),
         ),
-        // Change App bar bg color
-        backgroundColor: const Color.fromARGB(255, 81, 61, 236),
-        // Remove the shadow of the app bar
-        elevation: 0.0,
-        // Center the text
-        centerTitle: true,
-        leading: GestureDetector(
+      ),
+      actions: [
+        GestureDetector(
           onTap: () {},
           child: Container(
             margin: const EdgeInsets.all(10),
             alignment: Alignment.center,
+            width: 20,
             decoration: BoxDecoration(
                 color: const Color(0x000000ff),
                 borderRadius: BorderRadius.circular(10)),
             child: SvgPicture.asset(
-              'assets/icons/Arrow - Left 2.svg',
+              'assets/icons/dots.svg',
               // ignore: deprecated_member_use
               color: Colors.white,
               height: 30,
@@ -42,27 +67,7 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ),
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 20,
-              decoration: BoxDecoration(
-                  color: const Color(0x000000ff),
-                  borderRadius: BorderRadius.circular(10)),
-              child: SvgPicture.asset(
-                'assets/icons/dots.svg',
-                // ignore: deprecated_member_use
-                color: Colors.white,
-                height: 30,
-                width: 30,
-              ),
-            ),
-          ),
-        ],
-      ),
+      ],
     );
   }
 }
