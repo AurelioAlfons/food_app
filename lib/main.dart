dd8b6f3cadcd115f5485d35b5bf63a6be2b65dea
AurelioAlfons <yuroalfons0407@gmail.com>
2024-09-24 15:32:53 +1000

    Add run

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/pages/home.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
