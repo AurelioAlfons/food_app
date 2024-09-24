ac51ad34520788be067f41a28c4b3d19fad8f70e
AurelioAlfons <yuroalfons0407@gmail.com>
2024-09-24 20:51:03 +1000

    Add the other icons

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -10,4 +10,13 @@ class CategoryModel {
     required this.iconPath,
     required this.boxColor,
   });
+
+  List<CategoryModel> getCategories() {
+    List<CategoryModel> categories = [];
+
+    categories.add(CategoryModel(
+        name: 'Salad',
+        iconPath: 'assets/icons/plate.svg',
+        boxColor: const Color(0xff92A3FD)));
+  }
 }
